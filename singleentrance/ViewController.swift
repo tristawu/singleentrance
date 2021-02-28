91fcf2be2cbbafea111fe4d1786d832098540fb6
VIncent <chengsheng.chen@fubon.com>
2021-02-28 10:37:53 +0800

    Initial Commit

---
+++ singleentrance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  singleentrance
+//
+//  Created by A9392 on 2021/2/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
